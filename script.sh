74693895b2bc8d78cc9e3b83d974b16f6c43cde6
root <root@localhost.localdomain>
2018-07-16 16:07:08 +0200

    Controle de l'existance du compte dans la BDD

--- script.sh
+++ script.sh
@@ -2,23 +2,28 @@
 
 grep /home/ /etc/passwd > /tmp/tpmfile
 
-database()
-{
+hote_db="localhost"
+login_db="root"
+nom_db="projet_linux"
+nom_table="users"
 
-    hote_db="localhost"
-    login_db="root"
-    nom_db="projet_linux"
-    nom_table="users"
+insert_system_users()
+{
     query1="CREATE DATABASE IF NOT EXISTS projet_linux;"
     query2="CREATE TABLE IF NOT EXISTS users (
-                id INT(6) AUTO_INCREMENT PRIMARY KEY,
+                uid INT(10) PRIMARY KEY,
                 nom VARCHAR(30),
-                uid VARCHAR(30),
                 gid VARCHAR(50),
                 home VARCHAR(30),
                 shell VARCHAR(30),
                 taille VARCHAR(50),
                 fingerprint VARCHAR(50));"
+    
+    #A utiliser pour supprimer la table:
+    queryDELETE="DROP TABLE users;"
+   
+    mysql -h "$hote_db" -u "$login_db" -D "$nom_db" -e "$query1"
+    mysql -h "$hote_db" -u "$login_db" -D "$nom_db" -e "$query2"
 
     for i in `cut -d: -f1,3,4,6,7 /tmp/tpmfile`
 do
@@ -29,20 +34,27 @@ do
     gid=$(cut -d: -f3 /tmp/tmpfile)
     shell=`cut -d: -f5 /tmp/tmpfile`
 
-    echo $home
-    echo $nom
-
-    query3="insert into users values ('', '$nom', '$uid', '$gid', '$home', '$shell', '$taille', '$fingerprint')"
-    mysql -h "$hote_db" -u "$login_db" -D "$nom_db" -e "$query3"
-
-
-    if [ -d $home ]
+    if [[ -z "$(userExistinDB $uid)" ]]
     then
-#	echo "taille du dossier : "
-	taille=`du -s $home 2>/dev/null`
-#	echo $taille
+	echo "J'ajoute l'user $uid dans la bdd"
+        query3="insert into users values ('$uid', '$nom', '$gid', '$home', '$shell', '$taille', '$fingerprint')"
+	mysql -h "$hote_db" -u "$login_db" -D "$nom_db" -e "$query3"
     fi
+
 done
 }
-#main
-database
+
+userExistinDB()
+{
+   uid=$1
+   query4="SELECT uid FROM users WHERE uid=$uid;"
+   mysql -h "$hote_db" -u "$login_db" -D "$nom_db" -e "$query4"
+}
+
+
+
+
+#userExistinDB 100000
+insert_system_users
+rm /tmp/tmpfile
+
