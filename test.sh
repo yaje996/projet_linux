0ad3933f5aa1b59e4927f27bf87f81f484ae1cec
yaje996 <yaje996@users.noreply.github.com>
2018-07-18 11:30:40 +0200

    script rapport

--- test.sh
+++ test.sh
@@ -3,18 +3,29 @@
 #tail /var/log/messages > $1
 cat /var/log/messages > /tmp/rapport.txt
 
-egrep '/etc|passwd|aptitude|install|su|sudo|/etc/shadow|export|kill|userdel|wget|ssh|chgrp|chown|chmod|root|env|path|root|useradd|adduser|iptables' /tmp/rapport.txt > rapport
+egrep '/etc|passwd|aptitude|install|su|sudo|/etc/shadow/|export|chmod|chown|env|export' /tmp/rapport.txt > rapport
 
-#if [ $# == 1 ]
-#then
-# egrep '/etc|passwd|aptitude|install|su|sudo|/etc/shadow|export|kill|userdel|wget|ssh|chgrp|chown|chmod|root|env|path|root|useradd|adduser|iptables' rapport.txt> tt
+hote_db="localhost"
+login_db="root"
+nom_db="projet_linux"
+nom_table="commande"
 
-#    date=$(cut -d' ' -f1,2,3 $1)
- #   user=$(cut -d' ' -f5 $1 | cut -d'[' -f1)
-  #  path=$(cut -d'[' -f5 $1 | cut -d']' -f1)
-   # command=$(cut -d: -f4 $1)
 
-#else
- #   echo "mettre en argument le fichier rapport"
-#fi
+query1="CREATE DATABASE IF NOT EXISTS projet_linux;"
+query2="CREATE TABLE IF NOT EXISTS commande (
+                date VARCHAR(30),
+                nom VARCHAR(30),
+                path VARCHAR(50),
+                command VARCHAR(50));"
 
+for i in `cut -d' ' -f1 rapport`
+do
+    echo $i > tmp
+
+    #date=$(cut -d' ' -f1,2,3 rapport)
+    user=$(cut -d' ' -f5 rapport | cut -d'[' -f1)
+   # path=$(cut -d'[' -f5 rapport | cut -d']' -f1)
+    #command=$(cut -d: -f4 rapport)
+done
+
+echo $user
